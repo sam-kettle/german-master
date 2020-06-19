1c82319c7b2f458bf6e365698033307825bde4de
Sam Kettle <sam-kettle@outlook.com>
2020-06-19 21:26:27 +0100

    created async generateNewNoun function to clean up code, app now generates a new noun when user enters correct answer

--- routes/noun-gender.js
+++ routes/noun-gender.js
@@ -2,22 +2,28 @@ const express = require('express')
 const Noun = require('../models/noun')
 const router = express.Router()
 
+// Route functions
+async function generateNewNoun(nounSchema) {
+    const count = await nounSchema.countDocuments()
+    const random = Math.floor(Math.random() * count)
+    return await nounSchema.findOne().skip(random)
+}
+
 router.get('/', function(req, res) {
-    Noun.countDocuments().exec((err, count) => {
-        const random = Math.floor(Math.random() * count)
-        Noun.findOne().skip(random).exec((e, nouns) => {
-            res.render('noun-gender', {title: 'Noun gender quiz', noun: nouns.noun})
-        })
+    generateNewNoun(Noun).then(r => {
+        res.render('noun-gender', {title: 'Noun gender quiz', noun: r.noun})
     })
 })
 
 router.post('/', (req, res) => {
     Noun.findOne({ noun: req.body.currentnoun }).exec((e, result) => {
         if (result.gender === req.body.userinput) {
-            res.render('noun-gender', {
-                title: 'Noun gender quiz',
-                noun: req.body.currentnoun,
-                answer: 'Correct!'
+            generateNewNoun(Noun).then(r => {
+                res.render('noun-gender', {
+                    title: 'Noun gender quiz',
+                    noun: r.noun,
+                    answer: 'Correct!'
+                })
             })
         } else {
             res.render('noun-gender', {

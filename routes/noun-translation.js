4ceda3e29e0fb94dd4ae6945ba9ac0cac2384963
Sam Kettle <sam-kettle@outlook.com>
2020-06-24 23:10:31 +0100

    User can select translation mode on page

--- routes/noun-translation.js
+++ routes/noun-translation.js
@@ -1,35 +1,27 @@
 const express = require('express')
 const Noun = require('../models/noun')
 const functions = require('./function')
+const { generateNewNoun } = require('./function')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    functions.generateNewNoun(Noun).then(r => {
-        res.render('noun-translation', {
-            title: 'Noun translation quiz',
-            noun: r.noun
-        })
-    })
+
+router.get('/', function(req, res) {
+    res.render('noun-translation', {title: 'Noun translation quiz', noun: 'Select Mode'})
 })
 
 router.post('/', (req, res) => {
-    Noun.findOne({ noun: req.body.currentnoun }).exec((e, result) => {
-        if (result.translation === req.body.userinput) {
-            functions.generateNewNoun(Noun).then(r => {
-                res.render('noun-translation', {
-                    title: 'Noun translation quiz',
-                    noun: r.noun,
-                    answer: 'Correct!'
-                })
-            })
-        } else {
-            res.render('noun-translation', {
-                title: 'Noun translation quiz',
-                noun: req.body.currentnoun,
-                answer: 'Incorrect'
-            })
-        }
-    })
+
+    if (req.body.mode === 'english-to-german') {
+        generateNewNoun(Noun).then(r => {
+            res.render('noun-translation', {title: 'Noun translation quiz', noun: r.translation})
+        })        
+    } else if (req.body.mode === 'german-to-english') {
+        generateNewNoun(Noun).then(r => {
+            res.render('noun-translation', {title: 'Noun translation quiz', noun: r.noun})
+        })    
+    } else {
+        res.render('noun-translation', {title: 'Noun translation quiz', noun: 'Select Mode'})
+    }
 })
 
 module.exports = router
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,6 @@
 let displayedNoun = document.getElementById('random-noun').textContent
 document.getElementById('currentnoun').value = displayedNoun
 
-if (document.URL.includes('noun-gender')) {
-    let translation = document.querySelector('#translation-text').textContent
-    document.querySelector('#translation').value = translation
-}
-
 const answerPopupText = document.getElementById('answer-popup-text')
 if (answerPopupText.textContent === 'Correct!') {
     answerPopupText.classList.add('correct')
@@ -17,4 +12,3 @@ setTimeout(() => {
     const popup = document.querySelector('#answer-popup-text')
     popup.classList.add('hidden')
 }, 1000)
-
